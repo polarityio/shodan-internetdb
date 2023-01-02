cfa2174f3e36a1cece2a80b8aaca6e3a171b99fc
Ed <ed@polarity.io>
2023-01-02 11:44:21 -0500

    Remove unused option validation helper files

--- src/userOptions/index.js
+++
@@ -1,4 +0,0 @@
-// const validateOptions = require('./validateOptions');
-// const parseUserOptionLists = require('./parseUserOptionLists');
-
-// module.exports = { validateOptions, parseUserOptionLists };
